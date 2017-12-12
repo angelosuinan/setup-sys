89359fcc501b65df085d254bdf3c6d324a2f5769
angelosuinan <suinanangelo@gmail.com>
2017-12-12 22:01:12 +0800

    update DATABASE

--- start.bat
+++ start.bat
@@ -8,7 +8,7 @@ ECHO .................STARTING SERVER.................
 cd web/src/
 
 pip install -r requirements.txt
-python manage.py runserver 0.0.0.0:80
+python manage.py runserver 0.0.0.0:8000
 
 PAUSE
 ECHO .................DONE.................
