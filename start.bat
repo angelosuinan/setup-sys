7c43855ea980c52396c8216642910a5687e47dfe
angelosuinan <suinanangelo@gmail.com>
2017-12-14 10:49:40 +0800

    update config files

--- start.bat
+++ start.bat
@@ -1,15 +1,1 @@
-@ECHO OFF
-ECHO.
-ECHO .................START SERVER.................
-ECHO.
-
-
-ECHO .................STARTING SERVER.................
-cd web/src/
-
-pip install -r requirements.txt
-python manage.py runserver 0.0.0.0:8000
-
-PAUSE
-ECHO .................DONE.................
-CLS
+start /min server.bat
