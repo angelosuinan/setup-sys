351d4835a03f5c9eab593c377cc58ae12f4783ee
angelosuinan <suinanangelo@gmail.com>
2018-01-11 14:01:43 +0800

    update config files

--- backup database.bat
+++ backup database.bat
@@ -28,6 +28,12 @@ git add -A .
 git commit -m "update DATABASE"
 git push origin production
 
+ECHO .................BACKUP FINGERPRINT.................
+cd C:\fingers\
+git add -A .
+git commit -m "update Fingerprint"
+git push origin master
+
 PAUSE
 
 ECHO .................SUCCESS.................
