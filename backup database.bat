c56b9a54004c3e752dd224acf63ad932584d3ef4
angelosuinan <suinanangelo@gmail.com>
2018-04-20 11:03:03 +0800

    update config files

--- backup database.bat
+++ backup database.bat
@@ -1,6 +1,35 @@
+@ECHO OFF
+ECHO.
+ECHO .................UPDATE CODE.................
+ECHO.
+PAUSE
+
+ECHO .................UPDATING CONFIG FILES.................
+git config --global user.email "suinanangelo@gmail.com"
+git config --global user.name "angelosuinan"
+git checkout production
+git pull origin production
+
+ECHO .................BACKUP CONFIG FILES.................
+git add -A .
+git commit -m "update config files"
+git push origin production
+
+ECHO .................UPDATING WEB FILES.................
+cd web/
+
+git checkout production
+git pull origin production
+
+
+
+ECHO .................BACKUP WEB FILES.................
+git add -A .
+git commit -m "update DATABASE"
+git push origin production
 
 ECHO .................BACKUP FINGERPRINT.................
-cd C:/fingers/
+cd /d C:/fingers/
 dir
 git add -A .
 git commit -m "update Fingerprint"
